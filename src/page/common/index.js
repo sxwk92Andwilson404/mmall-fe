508535522eb4677bc1784535222c007e75d0b725
sxwk92 <sxwenkai@163.com>
2017-06-28 16:44:39 +0800

    4-6

---
+++ src/page/common/index.js
@@ -0,0 +1,10 @@
+/*
+* @Author: Administrator
+* @Date:   2017-06-25 00:33:21
+* @Last Modified by:   sxwk92
+* @Last Modified time: 2017-06-28 14:27:36
+*/
+
+'use strict';
+
+require('./layout.css');
---
+++ src/page/module.js
@@ -0,0 +1,11 @@
+/*
+* @Author: Administrator
+* @Date:   2017-06-25 00:22:53
+* @Last Modified by:   sxwk92
+* @Last Modified time: 2017-06-25 00:26:09
+*/
+
+'use strict';
+module.exports = {
+	text:1234
+}
