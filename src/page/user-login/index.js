f54bda497654440e399c6fb3f5af1d3d78ed743b
sxwk92 <sxwenkai@163.com>
2017-07-01 23:44:03 +0800

    test

---
+++ src/page/user-login/index.js
@@ -0,0 +1,11 @@
+/*
+* @Author: Administrator
+* @Date:   2017-06-24 18:18:24
+* @Last Modified by:   sxwk92
+* @Last Modified time: 2017-06-30 11:17:43
+*/
+
+'use strict';
+require('./index.css');
+require('page/common/nav-simple/index.js');
+var _mm = require('util/mm.js');
