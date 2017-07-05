ba36e33e97e3f4828800d3e52c5a52862e6d4111
sxwk92 <sxwenkai@163.com>
2017-07-05 22:43:23 +0800

    6-2

--- src/page/index/index.js
+++ src/page/index/index.js
@@ -2,15 +2,16 @@
 * @Author: sxwk92
 * @Date:   2017-06-24 02:09:55
 * @Last Modified by:   sxwk92
-* @Last Modified time: 2017-06-29 21:38:08
+* @Last Modified time: 2017-07-05 22:42:14
 */
 'use strict';
+require('./index.css');
 require('page/common/nav/index.js');
 require('page/common/header/index.js');
 var navSide = require('page/common/nav-side/index.js');
 var _mm = require('util/mm.js');
 
-navSide.init({
-	name:'user-center'
-});
+
+
+
 
